193f44ddea8ebefec2f2384a0c87da57b8f13d07
Gayasuddin <gyasuansari145@gmail.com>
2023-11-24 10:54:19 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+const t1 = document.getElementById("t1");
+const navMenu = document.querySelector(".nav-menu");
+
+t1.addEventListener('click',function barchange(){
+    t1.classList.toggle('change')
+    navMenu.classList.toggle('active')
+})
+
+// document.querySelectorAll('.nav-link').forEach(n => n.addEventListener('click', () => {
+//     t1.classList.remove('change')
+//     navMenu.classList.remove('active')
+// }))
+
+document.onclick = function(event){
+    if (!navMenu.contains(event.target) && !t1.contains(event.target) ) {
+        navMenu.classList.remove("active");
+        t1.classList.remove("change");
+    }
+}
